24c119d30730bd3f8c45e4a91e4885c388a3fb02
Aykuli <aykuli@ya.ru>
2020-02-16 14:38:54 +0700

    feat: add template to the blog with graphQL API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,46 @@
-module.exports = () => {
-  console.log("111")
+const path = require("path")
+
+module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+    console.log("-----------------------------------------------")
+    console.log("\n\n", slug)
+    console.log("-----------------------------------------------")
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
+
+module.exports.createPage = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const blogTemplate = path.resolve(`./srsc/templates/blog.js`)
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}/`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
 }
